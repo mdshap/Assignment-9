b3b3c2b2ee2a0aa0037c898d995d4c02bd8361e9
mdshap <notunmdshaptarshi@gmail.com>
2025-12-06 13:19:58 +0600

    .env.local file added and secured the api keys

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOy_Tb0M1Q69yqBZLvRBSMhw2SpbJMjQA",
-  authDomain: "smart-db-user.firebaseapp.com",
-  projectId: "smart-db-user",
-  storageBucket: "smart-db-user.firebasestorage.app",
-  messagingSenderId: "497069293024",
-  appId: "1:497069293024:web:e5a9a91c0834d568febf85"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
+
 export const auth = getAuth(app);
